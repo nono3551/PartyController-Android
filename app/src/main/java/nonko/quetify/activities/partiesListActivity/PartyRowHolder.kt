98e0aacb73093db2e5324456fcbe5aac2067b454
Michal Záhradník <nono3551@gmail.com>
2020-07-28 20:48:38 +0200

    Removed old project

--- app/src/main/java/nonko/quetify/activities/partiesListActivity/PartyRowHolder.kt
+++
@@ -1,6 +0,0 @@
-package nonko.quetify.activities.partiesListActivity
-
-import android.support.v7.widget.RecyclerView
-import android.widget.TextView
-
-class PartyRowHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
--- app/src/main/java/nonko/quetify/entities/QueueRequest.kt
+++
@@ -1,3 +0,0 @@
-package nonko.quetify.entities
-
-data class QueueRequest(val secret: String, val songLink: String, val targetToken: String?)
