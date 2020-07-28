ce2d36ee18ad576d64cca686ddaf42ccb0090cf9
Michal Záhradník <nono3551@gmail.com>
2020-07-28 22:25:36 +0200

    Added activity and enahnced ui

--- app/src/main/java/com/nonko/partyremote/ui/main/MainActivity.kt
+++ app/src/main/java/com/nonko/partyremote/ui/main/MainActivity.kt
@@ -1,10 +1,26 @@
 package com.nonko.partyremote.ui.main
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
 import com.nonko.partyremote.R
 import com.nonko.partyremote.ui.base.ToolbarActivity
+import com.nonko.partyremote.ui.create_party_session.CreatePartySessionActivity
+import sk.backbone.android.shared.utils.setSafeOnClickListener
 
 class MainActivity : ToolbarActivity() {
+    override fun getRightView(): View? {
+        return ImageView(this).apply {
+            layoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+            setImageResource(android.R.drawable.ic_input_add)
+            setSafeOnClickListener {
+                CreatePartySessionActivity.startActivity(this@MainActivity)
+            }
+        }
+    }
+
     override fun getActivityLayoutId(): Int? {
         return R.layout.activity_main
     }
