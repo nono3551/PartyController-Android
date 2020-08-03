4ffb07ae8eaf094239587a73ffd5523ae5387d72
Michal Záhradník <nono3551@gmail.com>
2020-08-03 19:20:29 +0200

    Examples

--- app/src/main/java/com/partyremote/android/ui/main/MainActivity.kt
+++ app/src/main/java/com/partyremote/android/ui/main/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.partyremote.android.ui.main
 
 import android.os.Bundle
+import android.text.Layout
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import com.partyremote.android.R
 import com.partyremote.android.ui.base.ToolbarActivity
 import com.partyremote.android.ui.create_party_session.CreatePartySessionActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import sk.backbone.android.shared.utils.setSafeOnClickListener
+import java.util.zip.Inflater
 
 class MainActivity : ToolbarActivity() {
     override fun getCenterView(): View? {
@@ -27,5 +33,13 @@ class MainActivity : ToolbarActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+        layoutInflater.inflate(R.layout.ui_party_session_view, test).apply { this.layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)}
+        layoutInflater.inflate(R.layout.ui_party_session_view, test).apply { this.layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)}
+        layoutInflater.inflate(R.layout.ui_party_session_view, test).apply { this.layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)}
+        layoutInflater.inflate(R.layout.ui_party_session_view, test).apply { this.layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)}
+        layoutInflater.inflate(R.layout.ui_party_session_view, test).apply { this.layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)}
     }
 }
